e94b81a6b2927e370dfb2ebb6d34a35995a1a871
romainm <romainm@MBP-de-Romain.lan>
2023-05-10 22:36:04 +0200

    [ADD] admin comp

---
+++ src/components/Admin/organisms/Admin.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h1>login</h1>
+
+  <form class="admin-form">
+    <input
+      class="admin-input"
+      type="text"
+      name="username"
+      required
+      placeholder="username"
+    />
+    <input
+      class="admin-input"
+      type="password"
+      name="password"
+      required
+      placeholder="password"
+    />
+    <button class="admin-btn">go</button>
+  </form>
+</div>
