b626dba4d3cc412e05d6a2f0b1bbd3f83e4b1871
romainm <romainm@MBP-de-Romain.lan>
2023-05-10 22:34:50 +0200

    [ADD] toast

---
+++ src/components/Toasts/organisms/Toast.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { ToastStatus } from "../../../types/Toast.d";
+
+  export let status: ToastStatus;
+  export let message: string;
+</script>
+
+{#if status === ToastStatus.success}
+  <div>
+    <p>success !</p>
+    <p>{message}</p>
+  </div>
+{:else if status === ToastStatus.info}
+  <div>
+    <p>a thing happened</p>
+    <p>{message}</p>
+  </div>
+{:else if status === ToastStatus.warning}
+  <div>
+    <p>warning !</p>
+    <p>{message}</p>
+  </div>
+{:else}
+  <div>
+    <p>an error has occurred</p>
+    <p>{message}</p>
+  </div>
+{/if}
