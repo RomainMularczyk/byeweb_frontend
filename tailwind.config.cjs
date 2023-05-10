33c7f0838dff63e8214d3fb94935a319d5085fc9
Romain Mularczyk <romain.mularczyk@gmail.com>
2023-05-11 00:19:06 +0200

    [ADD] custom colors & fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ["ui-sans-serif", "tahoma"],
-      serif: ["ui-serif", "tahoma"],
+      sans: ["Carbono", ...defaultTheme.fontFamily.sans],
+      // serif: ["ui-serif", "tahoma"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "dark-primary": "#212121",
+        "dark-secondary": "#474747",
+        "light-primary": "#FEFEFE",
+        "light-secondary": "#E4E4E4",
+      },
+    },
   },
   plugins: [],
 };
